89f92d7f9cb72c118b7f6be227364d786f254d74
psychopo <psychopo@icloud.com>
2025-07-12 18:00:03 +0700

    added callback & logic with callback

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,6 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 import app.keyboards as kb
 
@@ -9,24 +9,32 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("Привет!", reply_markup=kb.settings)
+    await message.answer('Привет!', reply_markup=kb.settings)
 
 
-@router.message(Command("help"))
+@router.message(Command('help'))
 async def get_help(message: Message):
-    await message.answer("Вы воспользовались командой /help!")
+    await message.answer('Вы воспользовались командой /help!')
 
 
-@router.message(F.text.lower() == "привет")
+@router.message(F.text.lower() == 'привет')
 async def answer_hi(message: Message):
-    await message.reply(f"Пользователь {message.from_user.first_name} с ID:{message.from_user.id} я рад, что вы нашли меня!",
+    await message.reply(f'Пользователь {message.from_user.first_name} с ID:{message.from_user.id} я рад, что вы нашли меня!',
 	 							reply_markup=kb.main)
 
 
 @router.message(F.photo)
 async def get_photo(message: Message):
-    await message.answer(f"ID этого фото: {message.photo[-1].file_id}")
+    await message.answer(f'ID этого фото: {message.photo[-1].file_id}')
     await message.answer_photo(photo=message.photo[-1].file_id, 
-                            caption="Я тоже могу это отправить вам!")
-    await message.answer_photo(photo="https://assets.monica.im/home-web/_next/static/media/chatPdfTranslation.9c971641.png",
-                            caption="А еще вот это!")
+                            caption='Я тоже могу это отправить вам!')
+    await message.answer_photo(photo='https://assets.monica.im/home-web/_next/static/media/chatPdfTranslation.9c971641.png',
+                            caption='А еще вот это!')
+
+
+@router.callback_query(F.data == 'about_me')
+async def about_me(callback: CallbackQuery):
+    await callback.answer('') #('Вы выбрали "Обо мне"', show_alert=True)
+    # await callback.message.answer('Я -- бот созданный PsychoPo!\nЯ нахожусь в разработке, поэтому некоторые мои функции могут не работать!')
+    await callback.message.edit_text('Я -- бот созданный PsychoPo!\nЯ нахожусь в разработке, поэтому некоторые мои функции могут не работать!',
+                                reply_markup=await kb.inline_cars())
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,15 +1,33 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
 main = ReplyKeyboardMarkup(keyboard=[
-	[KeyboardButton(text="Обо мне")],
-	[KeyboardButton(text="Какая погода сейчас в Красноярске?"), KeyboardButton(text="Какая погода сейчас в Москве")],
-	[KeyboardButton(text="Закрыть")]
+	[KeyboardButton(text='Обо мне')],
+	[KeyboardButton(text='Какая погода сейчас в Красноярске?'), KeyboardButton(text='Какая погода сейчас в Москве')],
+	[KeyboardButton(text='Закрыть')]
 ],
 	resize_keyboard=True,
-	input_field_placeholder="Выберите пункт меню.")
+	input_field_placeholder='Выберите пункт меню.')
 
 
 settings = InlineKeyboardMarkup(inline_keyboard=[
-			[InlineKeyboardButton(text="Переводчик", url="https://www.deepl.com/en/translator"),
-			InlineKeyboardButton(text="Youtube", url="https://www.youtube.com/")]
+			[InlineKeyboardButton(text='Переводчик', url='https://www.deepl.com/en/translator'),
+			InlineKeyboardButton(text='Youtube', url='https://www.youtube.com/'),
+			InlineKeyboardButton(text='Обо мне', callback_data='about_me')]
 			])
+
+
+cars_from_bd = ['Mazda', 'BMW', 'Kio', 'Mercedes', 'Hyndai', 'Honda']
+
+# async def reply_cars():
+# 	keyboard = ReplyKeyboardBuilder()
+# 	for car in cars_from_bd:
+# 		keyboard.add(KeyboardButton(text=car))
+# 	return keyboard.adjust(2).as_markup()
+
+async def inline_cars():
+	keyboard = InlineKeyboardBuilder()
+	for car in cars_from_bd:
+		keyboard.add(InlineKeyboardButton(text=car, callback_data=car))
+	return keyboard.adjust(2).as_markup()
